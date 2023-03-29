7f7105379ad3c1b4413c8fc3f933fddcbf0507ba
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-03-29 14:30:06 -0300

    Consulta simples com @Query

--- src/main/java/com/rogeriofrsouza/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/rogeriofrsouza/workshopmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.rogeriofrsouza.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.rogeriofrsouza.workshopmongo.domain.Post;
 
@@ -13,4 +14,7 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	 * Busca posts contendo um dado string no título, ignora case sensitive
 	 */
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")  // ?0 -> valor recebido por parâmetro (text)
+	List<Post> searchTitle(String text);
 }
